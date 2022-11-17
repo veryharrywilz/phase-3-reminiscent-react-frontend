332123731bba6f7c4a11a1f4e47ebffadd416852
madelinedulchin <madeline.dulchin@gmail.com>
2022-11-16 20:54:04 -0500

    centering cards

--- src/components/CandleContainer.js
+++ src/components/CandleContainer.js
@@ -13,7 +13,7 @@ function CandleContainer({ candleArr, setCart, checkOut, currentUser }) {
 
     return (
 
-        <div>
+        <div className="displayCandles">
             {allCandles}
         </div>
     )
